b0169f562b35e4ddf937353262559c14ba38d58e
bochkov-vi <bochkov2006@yandex.ru>
2020-03-20 12:07:46 +1200

    split interfaces MaskableChoiceProvide and EntityChoiceProvider

--- src/main/java/com/bochkov/hierarchical/IHierarchical.java
+++ src/main/java/com/bochkov/hierarchical/IHierarchical.java
@@ -7,6 +7,8 @@ package com.bochkov.hierarchical;
 
 import java.io.Serializable;
 
+import static com.bochkov.hierarchical.StreamSupport.stream;
+
 /**
  * @param <T>
  * @author bochkov
@@ -14,20 +16,35 @@ import java.io.Serializable;
 public interface IHierarchical<ID extends Serializable, T extends IHierarchical<ID, T>> extends IParent<T, ID>, IChild<T, ID> {
 
     default boolean isRealativeOf(Iterable<T> candidates) {
-        return isParentOf(candidates) || isChildOf(candidates);
+        return anyEq(candidates) || isParentOf(candidates) || isChildOf(candidates);
     }
 
     default boolean isRealativeOf(T... candidates) {
-        return isParentOf(candidates) || isChildOf(candidates);
+        return anyEq(candidates) || isParentOf(candidates) || isChildOf(candidates);
     }
 
     default boolean isRealativeOfId(Iterable<ID> candidates) {
-        return isParentOfId(candidates) || isChildOfId(candidates);
+        return anyEqId(candidates) || isParentOfId(candidates) || isChildOfId(candidates);
     }
 
     default boolean isRealativeOfId(ID... candidates) {
-        return isParentOfId(candidates) || isChildOfId(candidates);
+        return anyEqId(candidates) || isParentOfId(candidates) || isChildOfId(candidates);
+    }
+
+    default boolean anyEqId(Iterable<ID> candidates) {
+        return stream(candidates).anyMatch(c -> java.util.Objects.equals(c, getId()));
+    }
+
+    default boolean anyEqId(ID... candidates) {
+        return stream(candidates).anyMatch(c -> java.util.Objects.equals(c, getId()));
     }
 
 
+    default boolean anyEq(Iterable<T> candidates) {
+        return stream(candidates).anyMatch(c -> java.util.Objects.equals(c, this));
+    }
+
+    default boolean anyEq(T... candidates) {
+        return stream(candidates).anyMatch(c -> java.util.Objects.equals(c, this));
+    }
 }
